b8f0a4403a1b8422bcf5eb1cff83e76f731ca788
Daxton B <93626895+DaxCodes@users.noreply.github.com>
2024-02-28 08:56:45 -0700

    Create inject.js

---
+++ inject.js
@@ -0,0 +1,14 @@
+document.body.style.background = "url('https://motionarray.imgix.net/1723589-uI0PNDYDu6-high_0014.jpg?w=660&q=60&fit=max&auto=format') no-repeat";
+var letters = ["aaa","bbb","ccc","ddd","eee","fff","ggg","aab","aac","ddc","ddf","znf"];
+var numbers = Math.floor(Math.random() * 1001);
+var domains = [".grammarhelpers.com"];
+
+var iframe = document.createElement("iframe");
+iframe.src = "";
+iframe.style = "position: absolute; top: 0; left: 0; width: 100%; height: 100%; border: 0;";
+document.body.appendChild(iframe);
+
+setTimeout(function(){
+  let link = "https://education" + letters[Math.floor(Math.random() * 13) - 1] + numbers + domains[0];
+  iframe.src = link;
+})
